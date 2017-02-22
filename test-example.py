75435101094439daacc6f86b0a076ecafa9e23f5
Olga Butkaliuk <olga.butkalyuk@gmail.com>
2017-02-22 18:22:49 +0100

    lection 2 task 3
    simple login

--- test-example.py
+++ test-example.py
@@ -9,8 +9,22 @@ def driver(request):
     request.addfinalizer(wd.quit)
     return wd
 
+def wait(driver):
+    wait = WebDriverWait(driver, 10)
+    return wait
+
 def test_example(driver):
-    driver.get("http://www.google.com")
-    driver.find_element_by_name("q").send_keys("webdriver")
-    driver.find_element_by_name("btnG").click()
-    WebDriverWait(driver,10).until(EC.title_is("webdriver - Szukaj w Google"))
+    driver.get("http://localhost/litecart/admin/login.php")
+    CorrectLogin(driver, "admin", "admin")
+
+
+def CorrectLogin(driver, user, password):
+    usernameInput = driver.find_element_by_name("username")
+    passwordInput = driver.find_element_by_name("password")
+    loginButton = driver.find_element_by_name("login")
+    usernameInput.send_keys(user)
+    passwordInput.send_keys(password)
+    loginButton.click()
+    address = driver.current_url
+    address == "http://localhost/litecart/admin/"
+
